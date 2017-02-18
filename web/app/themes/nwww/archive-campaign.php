7d4514c5c26374e30a405a1130ccac5e049ff3a8
Estr <e@estrma.net>
2017-02-18 13:59:25 +0100

    Single campaign layout

--- web/app/themes/nwww/archive-campaign.php
+++ web/app/themes/nwww/archive-campaign.php
@@ -5,53 +5,52 @@ get_header(); ?>
 
     <section class="padded section container c__w" id="content" role="main">
 
-    <div class="row">
-
-        <?php get_template_part('categories'); ?>
-
-        <div class="col-md-9 campaign__list">
-		<?php
-		if ( have_posts() ) : ?>
-            <div class="row">
+        <div class="row">
 
 
-			<?php
-
-			while ( have_posts() ) : the_post(); ?>
-                <article class="col-md-12 campaign__in__list">
-
-<?php
-
-        global $app;
-
-        $speakout = $app->get_speakout_info($post->ID);
-        $app->render('big-campaign', [
-            'signed' => $app->calc_perc($speakout),
-            'title' => get_the_title($post->ID),
-            'excerpt' => $app->excerpt_by_id($post->ID),
-            'link' => get_post_permalink($post->ID),
-            'img' => get_the_post_thumbnail($post->ID, 'mid'),
-          //   'children' => $app->get_children($post)
-        ]);
-
-?>
-
-                </article>
-
-			<?php endwhile;
-
-			the_posts_navigation();
-
-		else :
+            <div class="col-md-9 campaign__list">
+                <?php
+                if (have_posts()) : ?>
+                <div class="row">
 
 
+                    <?php
 
-		endif; ?>
+                    while (have_posts()) : the_post(); ?>
+                        <article class="col-md-12 campaign__in__list">
 
-		</div>
-		</div>
-		</div>
-	</section><!-- #primary -->
+                            <?php
+
+                            global $app;
+
+                            $speakout = $app->get_speakout_info($post->ID);
+                            $app->render('big-campaign', [
+                                'signed' => $app->calc_perc($speakout),
+                                'title' => get_the_title($post->ID),
+                                'excerpt' => $app->excerpt_by_id($post->ID),
+                                'link' => get_post_permalink($post->ID),
+                                'img' => get_the_post_thumbnail($post->ID, 'mid'),
+                                //   'children' => $app->get_children($post)
+                            ]);
+
+                            ?>
+
+                        </article>
+
+                    <?php endwhile;
+
+                    the_posts_navigation();
+
+                    else :
+
+
+                    endif; ?>
+
+                </div>
+            </div>
+            <?php get_template_part('categories'); ?>
+        </div>
+    </section><!-- #primary -->
 
 <?php
 
--- web/app/themes/nwww/single-campaign.php
+++ web/app/themes/nwww/single-campaign.php
@@ -1,12 +1,22 @@
-
 <?php get_header(); ?>
 <?php global $app;
 if (have_posts()) while (have_posts()) : the_post(); ?>
-    <section class="padded section container c__y" id="content" role="main">
+    <section class="padded section container " id="content" role="main">
 
-        <div class="row is-flex">
+        <div class="row  is-flex">
+            <div class="col-sm-6 col-md-6 col-lg-6 ">
+                <article class="c__w  exp">
 
-            <div class=" col-sm-6 col-md-4 col-lg-4 ">
+
+                    <?php $app->cat_link($post->ID); ?>
+                    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                    <div class="lead">  <?php the_excerpt(); ?></div>
+                    <a class="btn c__g t__w scroll__link" href="#dzialania">Nasze działania</a>
+
+                </article>
+
+            </div>
+            <div class=" col-sm-6 col-md-6 col-lg-6 ">
 
 
                 <?php
@@ -17,18 +27,40 @@ if (have_posts()) while (have_posts()) : the_post(); ?>
                 $app->render('campaign', [
                     'signed' => $app->calc_perc($speakout),
                     'link' => get_post_permalink($post->ID),
-                    'img' => get_the_post_thumbnail($post->ID, 'mid')
-                ]);  ?>
+                    'img' => get_the_post_thumbnail($post->ID, 'medium')
+                ]); ?>
 
+
+            </div>
+
+
+        </div>
+        <div class="separator"></div>
+        <div class="row is-flex">
+            <div class="col-sm-12 col-md-8 col-lg-8 campaingn__longtext text-justify">
+                <?php the_content(); ?>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4 ">
                 <?php
-                if( have_rows('actions') ):
+                if (have_rows('actions')):
                     echo '    <div class=" ">';
-                    while ( have_rows('actions') ) : the_row();
+                    while (have_rows('actions')) : the_row();
 
-                        if( get_row_layout() == 'campaign' ):
+                        if (get_row_layout() == 'campaign'):
                             $campaign_id = get_sub_field('id');
-                            $app->render('campaign-signed', [  'signed' => $app->get_signed($campaign_id) ]);
-                            echo sprintf('<h1 class="padded side__title c__w h3"><a href="%s">%s</a></h1>', get_post_permalink($campaign_id), get_the_title($campaign_id));
+
+                            $speakout = $app->get_speakout_info($campaign_id);
+                            $app->render('campaign', [
+                                'signed' => $app->calc_perc($speakout),
+                                'title' => get_the_title($campaign_id),
+                                'excerpt' => false,
+                                'link' => false,
+                                'img' => get_the_post_thumbnail($campaign_id, 'medium')
+                            ]);
+                            echo '<br>';
+
+                            //  $app->render('campaign', ['signed' => $app->get_signed($campaign_id)]);
+                            //  echo sprintf('<h1 class="padded side__title c__w h3"><a href="%s">%s</a></h1>', get_post_permalink($campaign_id), get_the_title($campaign_id));
 
                         endif;
                     endwhile;
@@ -36,106 +68,88 @@ if (have_posts()) while (have_posts()) : the_post(); ?>
                 endif;
 
                 ?>
-
-
             </div>
-
-            <div class="col-sm-6 col-md-8 col-lg-8 ">
-                <article class="c__w padded exp">
-
-
-
-                            <?php $app->cat_link($post->ID); ?>
-                            <h1> <a href="<?php the_permalink();?>"><?php the_title(); ?></a></h1>
-                            <?php the_content(); ?>
-
-
-
-                </article>
-
-            </div>
-
-
-
         </div>
 
     </section>
+
+
     <?php
-    if( have_rows('actions') ): ?>
-    <section class="children__campaigns padded section container c__w">
-    <div class=" campaign__list c__w">
-        <div class="row">
-            <div class="col-md-2">
-                <h1 class="side__title">Nasze działania</h1>
-            </div>
-            <div class="col-md-10">
-            <div class="row">
+    if (have_rows('actions')): ?>
+        <section class="children__campaigns padded section container c__w">
+            <div class=" campaign__list c__w">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="side__title padded" id="dzialania">Nasze działania</h1>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="row">
 
-                <?php
+                            <?php
 
 
+                            while (have_rows('actions')) : the_row();
 
-                    while ( have_rows('actions') ) : the_row();
+                                if (get_row_layout() == 'campaign'):
 
-                        if( get_row_layout() == 'campaign' ):
+                                    ?>
+                                    <article class="col-md-4 ">
+                                        <?php
+
+                                        $campaign_id = get_sub_field('id');
+
+
+                                        $speakout = $app->get_speakout_info($campaign_id);
+                                        $app->render('campaign', [
+                                            'signed' => $app->calc_perc($speakout),
+                                            'title' => get_the_title($campaign_id),
+                                            'excerpt' => false,
+                                            'link' => get_post_permalink($campaign_id),
+                                            'img' => get_the_post_thumbnail($campaign_id, 'medium')
+                                        ]);
+                                        ?>
+                                    </article>
+                                    <?php
+
+                                elseif (get_row_layout() == 'post'):
+
+                                    $post_id = get_sub_field('id');
+
+                                    $app->render('post', [
+                                        'width' => 4,
+                                        'title' => get_the_title($post_id),
+                                        'content' => get_the_excerpt($id),
+                                        'link' => get_post_permalink($post_id),
+                                        'time' => human_time_diff(get_post_time('U', $id), current_time('timestamp')) . ' temu'
+                                    ]);
+
+
+                                elseif (get_row_layout() == 'custom'):
+
+                                    $app->render('post', [
+                                        'width' => 4,
+                                        'title' => get_sub_field('title'),
+                                        'content' => get_sub_field('content'),
+                                        'img' => get_sub_field('img')['sizes']['mid']
+                                    ]);
+
+
+                                endif;
+
+
+                            endwhile;
+
 
                             ?>
-                <article class="col-md-12 campaign__in__list">
-                <?php
-
-                            $campaign_id = get_sub_field('id');
-
-                            $f = $app->get_campaign_fields($campaign_id);
-
-                            $app->render('big-campaign', $f);
-                ?>
-                </article>
-                        <?php
-
-                        elseif( get_row_layout() == 'post' ):
-
-                            $post_id = get_sub_field('id');
-
-                            $app->render('post', [
-
-                                    'title' => get_the_title($post_id),
-                                    'content' => get_the_excerpt($id),
-                                    'link' => get_post_permalink($post_id),
-                                    'time' => human_time_diff( get_post_time('U', $id), current_time('timestamp') ) . ' temu'
-                            ]);
 
 
+                        </div>
+                    </div>
 
-                        elseif( get_row_layout() == 'custom' ):
-
-                            $app->render('post', [
-
-                                'title' => get_sub_field('title'),
-                                'content' => get_sub_field('content'),
-                                'img' => get_sub_field('img')['sizes']['mid']
-                            ]);
-
-
-
-                         endif;
-
-
-
-                    endwhile;
-
-
-
-
-                ?>
-
-
-        </div>
-        </div>
-
-        </div>
-    </div>
-    </section>
-    <?php  endif; ?>
-                    <?php endwhile; ?>
+                </div>
+            </div>
+        </section>
+    <?php endif; ?>
+<?php endwhile; ?>
 
 <?php get_footer();
