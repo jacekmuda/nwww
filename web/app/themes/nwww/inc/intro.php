f8a275a9c474727d51722d6693933d37c4869c16
Estr <e@estrma.net>
2017-02-27 00:12:32 +0100

    Intro arrow

--- web/app/themes/nwww/inc/intro.php
+++ web/app/themes/nwww/inc/intro.php
@@ -9,5 +9,15 @@
     <div class="intro__inner">
         <h1><span class="c__w"><?php echo $first['lead']; ?></span></h1>
         <img src="<?php echo $first['photos']; ?>">
+        <div class="pointer">
+            <svg version="1.1" class="arrow" xmlns="http://www.w3.org/2000/svg"
+                 x="0px" y="0px"
+                 width="100px" height="50px" viewBox="0 0 100 50" enable-background="new 0 0 100 50"
+                 xml:space="preserve">
+<polygon fill="#FFFFFF" points="0,0 50,50 100,0 "/>
+</svg>
+        </div>
+
+
     </div>
 </section>
